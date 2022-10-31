784de0bb88cf16e153323048455561ea2b380464
AdisufatulAnam <203310019@students.akakom.ac.id>
2022-11-01 02:31:32 +0700

    update pada app bar dengan mengunakan component

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 //tambahkan import untuk memangil data dari main.js
 import main from './src/script/view/main.js';
+//tambahkan import untuk component 
+import './src/script/component/app-bar.js';
 document.addEventListener('DOMContentLoaded', main);
