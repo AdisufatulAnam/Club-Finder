3135fcad28ae7ad74655b6ead7d866b6a8189e6a
AdisufatulAnam <203310019@students.akakom.ac.id>
2022-11-07 21:38:49 +0700

    update pengunaan serch bar

--- src/script/component/app-bar.js
+++ src/script/component/app-bar.js
@@ -1,10 +1,10 @@
-class AppBar extends HTMLElement{
-    connectedCallback(){
+class AppBar extends HTMLElement {
+    connectedCallback() {
         this.render();
     }
-    render(){
-        this.innerHTML=`<h2>Club Finder</h2>`;
-        
+    render() {
+        this.innerHTML = `<h2>Club Finder</h2>`;
+
     }
 }
-customElements.define('app-bar',AppBar);
+customElements.define('app-bar', AppBar);
