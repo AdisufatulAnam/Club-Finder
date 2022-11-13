21443b8b4bb1307da1033ec1d821f286ea326f48
AdisufatulAnam <203310019@students.akakom.ac.id>
2022-11-14 01:33:46 +0700

    update pada file app.js untuk dengan import pada css

---
+++ src/app.js
@@ -0,0 +1,8 @@
+//tambahkan import untuk memangil data dari main.js
+import main from './src/script/view/main.js';
+//tambahkan import untuk component 
+import './src/script/component/app-bar.js';
+//import css 
+import './styles/style.css';
+
+document.addEventListener('DOMContentLoaded', main);
